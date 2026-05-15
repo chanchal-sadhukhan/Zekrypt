cbae89af63f877c509b8ec326501f52209cafcca
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2026-05-15 12:31:18 +0530

    feat: fetch data from frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+
+type FileRecord = {
+  id: number;
+  fileName: string;
+};
 
 export default function Home() {
+  const [files, setFiles] = useState<FileRecord[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/files")
+      .then((res) => res.json())
+      .then((data) => setFiles(data))
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello World!</h1>
-    </div>
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">
+        Zekrypt Files
+      </h1>
+
+      <div className="space-y-4">
+        {files.map((file) => (
+          <div
+            key={file.id}
+            className="border p-4 rounded"
+          >
+            {file.fileName}
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
